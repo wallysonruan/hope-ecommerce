f72127d83c3115ce8b4262ef6f3acb9dab6fd3de
Arthur Luna Borba <arthurlunabcf@outlook.com>
2022-08-01 19:09:28 -0300

    retificação da mensagem de validação na classe inventario request

--- src/main/java/dc/hope/request/InventarioRequest.java
+++ src/main/java/dc/hope/request/InventarioRequest.java
@@ -19,7 +19,7 @@ public class InventarioRequest {
     private Long produtoId;
 
     @NotNull(message = "É preciso informar Id do Pedido, Id do produto e quantidade desejada")
-    @Positive
+    @Positive(message = "É preciso informar Id do Pedido, Id do produto e quantidade desejada")
     private int quantidade;
     
 }
